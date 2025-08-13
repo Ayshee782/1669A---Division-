3823042cf06742df6bcc52fb131c4d5e89d2a5bd
Ayshee <152168245+Ayshee782@users.noreply.github.com>
2025-08-13 20:21:27 +0600

    Add files via upload

---
+++ 1669A-Division.py
@@ -0,0 +1,12 @@
+t = int(input())  # number of test cases
+
+for _ in range(t):
+    rating = int(input())
+    if rating >= 1900:
+        print("Division 1")
+    elif rating >= 1600:
+        print("Division 2")
+    elif rating >= 1400:
+        print("Division 3")
+    else:
+        print("Division 4")
